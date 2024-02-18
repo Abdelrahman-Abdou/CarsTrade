660b05bde5e57b4d80ba1e8415e20bd88219cc87
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-18 22:10:52 +0200

    add signout page

---
+++ pages/Signout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <div class="max-w-md w-full p-6 bg-white rounded-md shadow-md">
+            <h2 class="text-xl font-semibold mb-4">Sign out</h2>
+            <p class="text-gray-600 mb-6">Are you sure you want to sign out?</p>
+            <button @click="signout"
+                class="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300">
+                Sign out
+            </button>
+        </div>
+    </div>
+</template>
+  
+<script setup>
+const supabase = useSupabaseClient()
+const signout = async () => {
+    const { error } = await supabase.auth.signOut()
+
+}
+</script>
+  
--- pages/login.vue
+++ pages/login.vue
@@ -12,13 +12,13 @@ const login = async () => {
         console.log(data)
     }
 }
+const user = useSupabaseUser()
 </script>
 <template>
     <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
         </div>
-
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
                 <form class="space-y-6" @submit.prevent="login">
