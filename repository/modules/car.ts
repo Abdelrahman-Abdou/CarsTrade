af866d88596ba3ac3a80ebde707eae113e1a8a79
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-22 16:39:59 +0200

    add car api calls

--- repository/modules/car.ts
+++ repository/modules/car.ts
@@ -12,5 +12,49 @@ class carModule extends HttpFactory {
       },
     });
   }
+  async fetchUserListings(params: any): Promise<{ data: any }> {
+    console.log("fetchUserListings", params);
+    return await this.call({
+      url: `${this.Resource}/listings/user/${params.listerId}`,
+      options: {
+        method: "GET",
+      },
+    });
+  }
+  async deleteListing(params: any): Promise<{ data: any }> {
+    console.log("fetchUserListings", params);
+    return await this.call({
+      url: `${this.Resource}/listings/${params.listingId}`,
+      options: {
+        method: "DELETE",
+      },
+    });
+  }
+  async AddListing(params: any): Promise<{ data: any }> {
+    return await this.call({
+      url: `${this.Resource}/listings`,
+      options: {
+        method: "POST",
+        body: params,
+      },
+    });
+  }
+  async AddMessage(params: any): Promise<{ data: any }> {
+    return await this.call({
+      url: `${this.Resource}/listings/${params.listingId}/message`,
+      options: {
+        method: "POST",
+        body: params,
+      },
+    });
+  }
+  async getCarMessages(params: any): Promise<{ data: any }> {
+    return await this.call({
+      url: `${this.Resource}/listings/${params.listingId}/message`,
+      options: {
+        method: "GET",
+      },
+    });
+  }
 }
 export default carModule;
