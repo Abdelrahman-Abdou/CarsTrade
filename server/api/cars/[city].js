6045658f31ed0d887f4900ba1e19915bbffe69e8
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-22 16:41:07 +0200

    filter cars from server

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -1,24 +1,38 @@
-import cars from "@/data/cars.json";
-export default defineEventHandler((e) => {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export default defineEventHandler(async (e) => {
   const { make, minPrice, maxPrice } = getQuery(e);
   const { city } = e.context.params;
-  let filteredCars = cars.filter((car) => {
-    return car.city.toLowerCase() === city.toLowerCase();
-  });
+  const filters = {
+    city: city.toLocaleLowerCase(),
+  };
   if (make) {
-    filteredCars = filteredCars.filter((car) => {
-      return car.make.toLowerCase() === make.toLowerCase();
-    });
+    filters.make = make.toLocaleLowerCase();
+  }
+  if (minPrice || maxPrice) {
+    filters.price = {};
   }
   if (minPrice) {
-    filteredCars = filteredCars.filter((car) => {
-      return car.price >= parseInt(minPrice);
-    });
+    filters.price.gte = parseInt(minPrice);
   }
   if (maxPrice) {
-    filteredCars = filteredCars.filter((car) => {
-      return car.price < parseInt(maxPrice);
-    });
+    filters.price.lte = parseInt(maxPrice);
   }
-  return filteredCars;
+  const cars = await prisma.car.findMany({
+    where: filters,
+    select: {
+      id: true,
+      make: true,
+      model: true,
+      year: true,
+      price: true,
+      image: true,
+    },
+    orderBy: {
+      price: "asc",
+    },
+    take: 10,
+    skip: 0,
+  });
+  return cars;
 });
