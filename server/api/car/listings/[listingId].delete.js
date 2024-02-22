bc98bd59449ed800056bfd42747a55d82282ee51
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-22 16:42:39 +0200

    delete care from database

---
+++ server/api/car/listings/[listingId].delete.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export default defineEventHandler(async (e) => {
+  console.log(e.context.params.listingId);
+  await prisma.car.delete({
+    where:{
+      id: +e.context.params.listingId
+    }
+  })
+});
