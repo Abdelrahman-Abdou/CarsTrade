1a2db353f1705ad8374513f99d378d620771b80c
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-22 16:42:10 +0200

    fetch single care details

--- server/api/car/[car].js
+++ server/api/car/[car].js
@@ -1,15 +1,20 @@
-import cars from "@/data/cars.json";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export default defineEventHandler(async (e) => {
+  const id = e.context.params.car;
+  console.log(id, "from server");
+  const car = await prisma.car.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
 
-export default defineEventHandler((e) => {
-  const id = e.context.params.car
-  console.log(id, 'from server')
-  const car = cars.find((c) => c.id === parseInt(id));
   if (!car) {
     throw new createError({
       statusCode: 404,
       message: "Car not found",
-    })
+    });
   }
-  console.log(car)
+  console.log(car);
   return car;
 });
