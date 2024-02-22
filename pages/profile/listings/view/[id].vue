1aaffee64944da20c479e2e6ab40c68930223f75
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-22 16:39:24 +0200

    fetching msgs from database

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -3,13 +3,18 @@ definePageMeta({
   layout: "custom",
   middleware: ['auth']
 });
+const { $api } = useNuxtApp()
+const route = useRoute()
+const { data: messages } = useAsyncData('getMessgs', async () => {
+  const params = {
+    listingId: route.params.id
+  }
+  const res = await $api.car.getCarMessages(params)
+  return res
+})
 </script>
-
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
+    <CarMessageCard v-for="msg of messages" :msg="msg" />
   </div>
 </template>
