7e2f147eea1e185c3371dd76c5711f3e15b88839
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-22 16:40:28 +0200

    submit new listing from cliet side

--- components/Car/Detail/Contact.vue
+++ components/Car/Detail/Contact.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="mt-10">
-    <div class="flex w-[600px] justify-between">
-      <input type="text" class="border p-1" placeholder="Name" />
-      <input type="text" class="border p-1" placeholder="Email" />
-      <input type="text" class="border p-1" placeholder="Phone" />
+        <div class="flex w-[600px] justify-between">
+      <input type="text" class="border p-1" placeholder="Name" v-model="name" />
+      <input type="text" class="border p-1" placeholder="Email" v-model="email" />
+      <input type="text" class="border p-1" placeholder="Phone" v-model="phone" />
     </div>
     <div class="flex mt-4 w-[600px]">
-      <textarea class="border p-1 w-full" placeholder="Message"></textarea>
+      <textarea class="border p-1 w-full" placeholder="Message" v-model="message"></textarea>
     </div>
-    <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">
+    <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4" @click="submitHandler">
       Submit
     </button>
   </div>
 </template>
+<script setup>
+const route = useRoute()
+const listingId = ref(route.params.id)
+console.log(listingId)
+const name = ref()
+const email = ref()
+const phone = ref()
+const message = ref()
+
+const { $api } = useNuxtApp()
+const submitHandler = async () => {
+  const data = {
+    name: name.value,
+    email: email.value,
+    phone: phone.value,
+    message: message.value,
+    listingId: 4,
+
+  }
+  try {
+    await $api.car.AddMessage(data)
+    email.value = null
+    name.value = null
+    phone.value = null
+    message.value = null
+
+  } catch (error) {
+    throw createError({
+      statusCode: 400,
+      message: error.message,
+    })
+
+  }
+
+}
+
+
+</script>
